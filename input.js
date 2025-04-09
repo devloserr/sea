7fdc35bbae888911bf546f3c17ee485797f04be7
devloserr <laechotwo@gmail.com>
2025-04-09 15:13:51 -0300

    Update input.js

--- input.js
+++ input.js
@@ -1,1 +1,18 @@
+let output = document.getElementById('output')
 
+let delay = 100
+
+function update() {
+  const xhr = new XMLHttpRequest()
+
+  xhr.open('POST', 'output.php', true)
+  xhr.setRequestHeader('Content-Type: application/x-www-form-urlencoded')
+
+  xhr.onload = function() {
+    output.innerHTML += xhr.responseText
+  }
+
+  xhr.send('value=1');
+}
+
+setInterval(update, delay)
