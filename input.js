addebeb6f1758cb9a39864b77c33cecddbd2057d
devloserr <laechotwo@gmail.com>
2025-04-09 15:18:22 -0300

    Update input.js

--- input.js
+++ input.js
@@ -6,7 +6,7 @@ function update() {
   const xhr = new XMLHttpRequest()
 
   xhr.open('POST', 'output.php', true)
-  xhr.setRequestHeader('Content-Type: application/x-www-form-urlencoded')
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
 
   xhr.onload = function() {
     output.innerHTML += xhr.responseText
