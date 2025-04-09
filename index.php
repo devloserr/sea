3bea635e5eddc964eee08ab52282e64984944a82
devloserr <laechotwo@gmail.com>
2025-04-09 15:13:45 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
+<?php
+session_start();
+$_SESSION['times'] = 0;
+?>
+<!DOCTYPE html>
 
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Request test</title>
+</head>
+
+<body>
+  <h1 id="output" style="font-family: arial; font-weight: bold;"></h1>
+  <script src="input.js"></script>
+</body>
+
+</html>
